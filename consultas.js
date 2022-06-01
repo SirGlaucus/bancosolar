a718fd25b7baaa29dfc3a23740b8508c22c9b5cb
SirGlaucus <glaucusjoseph@gmail.com>
2022-05-31 22:18:18 -0400

    GET, usuarios(POST, GET y PUT) terminados

---
+++ consultas.js
@@ -0,0 +1,70 @@
+const { Pool } = require("pg")
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    password: "1234",
+    port: 5432,
+    database: "bancosolar"
+})
+
+// 1. Crear una ruta POST /cancion que reciba los datos correspondientes a una canción y realice a través de una función asíncrona la inserción en la tabla repertorio.
+const insertar = async (datos) => {
+    const consulta = {
+        text: "INSERT INTO usuarios(nombre, balance) VALUES ($1, $2) RETURNING *",
+        values: datos,
+    }
+    try {
+        const result = await pool.query(consulta)
+        return result.rows[0]
+    } catch (error) {
+        console.log(error.code)
+        return error
+    }
+}
+
+// 2. Crear una ruta GET /canciones que devuelva un JSON con los registros de la tabla repertorio.
+const consultar = async () => {
+    // Paso 2
+    try {
+        const result = await pool.query("SELECT * FROM usuarios")
+        return result.rows
+    } catch (error) {
+        // Paso 3
+        console.log(error.code)
+        return error
+    }
+}
+
+// 3. Crear una ruta PUT /cancion que reciba los datos de una canción que se desea editar, ejecuta una función asíncrona para hacer la consulta SQL correspondiente y
+// actualice ese registro de la tabla repertorio.
+const editar = async (datos) => {
+    const consulta = {
+        text: `UPDATE usuarios SET nombre = $1, balance = $2 WHERE id = $3 RETURNING *`,
+        values: datos,
+    }
+    try {
+        const result = await pool.query(consulta)
+        return result
+    } catch (error) {
+        console.log(error)
+        return error
+    }
+}
+
+// 4. Crear una ruta DELETE /cancion que reciba por queryString el id de una canción y
+// realiza una consulta SQL a través de una función asíncrona para eliminarla de la base de datos.
+const eliminar = async (id) => {
+    try {
+        const result = await pool.query(
+            `DELETE FROM repertorio WHERE id = '${id}'`
+        )
+        return result
+    } catch (error) {
+        console.log(error.code)
+        return error
+    }
+}
+
+
+
+module.exports = { insertar, consultar, editar, eliminar}
---
+++ index.js
@@ -0,0 +1,71 @@
+const http = require("http")
+const url = require('url')
+const fs = require('fs')
+
+const { insertar, consultar, editar, eliminar } = require("./consultas")
+
+http
+    .createServer(async (req, res) => {
+        // Lectural del archivo HTML para el servidor
+        if (req.url == "/" && req.method === "GET") {
+            res.setHeader("content-type", "text/html")
+            const html = fs.readFileSync("index.html", "utf8")
+            res.end(html)
+        }
+
+        // 1. Crear una ruta POST /cancion que reciba los datos correspondientes a una canción y realice a través de una función asíncrona la inserción en la tabla repertorio.
+        if ((req.url == "/usuario" && req.method === "POST")) {
+            let body = ""
+            req.on("data", (chunk) => {
+                body += chunk
+            })
+
+            req.on("end", async () => {
+                console.log(body)
+                const bodyObject = JSON.parse(body)
+                const datos = [bodyObject.nombre, bodyObject.balance]
+
+                const respuesta = await insertar(datos)
+
+                res.writeHead(201, { 'Content-Type': 'application/json' })
+                res.end(JSON.stringify(respuesta))
+            })
+        }
+
+        // 2. Crear una ruta GET /canciones que devuelva un JSON con los registros de la tabla repertorio.
+        if (req.url == "/usuarios" && req.method === "GET") {
+            const registros = await consultar()
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify(registros))
+        }
+
+        // 3. Crear una ruta PUT /cancion que reciba los datos de una canción que se desea editar, ejecuta una función asíncrona para hacer la consulta SQL correspondiente y
+        // actualice ese registro de la tabla repertorio.
+        if (req.url.startsWith("/usuario?") && req.method == "PUT") {
+            let body = ""
+            const { id } = url.parse(req.url, true).query
+
+            req.on("data", (chunk) => {
+                body += chunk
+            })
+
+            req.on("end", async () => {
+                const bodyObject = JSON.parse(body)
+                const datos = [bodyObject.name, bodyObject.balance, id]
+                const respuesta = await editar(datos)
+                res.writeHead(201, { 'Content-Type': 'application/json' })
+                res.end(JSON.stringify(respuesta))
+            })
+        }
+
+        // 4. Crear una ruta DELETE /cancion que reciba por queryString el id de una canción y
+        // realiza una consulta SQL a través de una función asíncrona para eliminarla de la base de datos.
+        if (req.url.startsWith("/usuario?") && req.method == "DELETE") {
+            // Paso 3
+            const { id } = url.parse(req.url, true).query
+            // Paso 4
+            const respuesta = await eliminar(id)
+            res.end(JSON.stringify(respuesta))
+        }
+    })
+    .listen(3000)
